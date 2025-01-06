eefb4ee0bd4baf55b55b73d3895354a6eb51934e
Jack Barber <jack@hellotechnology.co.uk>
2025-01-06 13:51:24 +0000

    Update settings_podcast.php

--- admin/templates/pages/settings/settings_podcast.php
+++ admin/templates/pages/settings/settings_podcast.php
@@ -11,7 +11,7 @@ perch_layout('header');
 $church = hello_church_church(true);
 $podcast = podcast();
 ?>
-<main class="flow">
+<main class="flow full big-little">
 	<h1>Podcast Settings</h1>
 	<div class="section-grid">
 		<div>
@@ -27,6 +27,16 @@ $podcast = podcast();
 					} 
 				?>
 			</section>
+			<div class="panel">
+				<h3>Notes</h3>
+				<ul>
+					<li>Podcasts are given the category 'Religion & Spirituality' and sub-category 'Christianity' by default</li>
+					<li>Podcasts are marked as 'not explicit' by default</li>
+					<li>The 'owner' of your podcast is <?= $church['churchEmail'] ?></li>
+				</ul>
+				<h3>Feed</h3>
+				<p class="monospace">https://app.hellochurch.tech/feed/<?= $church['churchSlug'] ?>/podcast.rss
+			</div>
 		</div>
 		<div>
 			<section>
@@ -35,11 +45,29 @@ $podcast = podcast();
 				</header>
 				<article>
 					<?php
-					$img = 'http://app.hellochurch.tech/feed/'.$church['churchSlug'].'/podcast.jpg';
+						function get_remote_file_info($url) {
+						    $ch = curl_init($url);
+						    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+						    curl_setopt($ch, CURLOPT_HEADER, TRUE);
+						    curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+						    $data = curl_exec($ch);
+						    $fileSize = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+						    $httpResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+						    curl_close($ch);
+						    return [
+						        'fileExists' => (int) $httpResponseCode == 200,
+						        'fileSize' => (int) $fileSize
+						    ];
+						}
+						
+						$img = 'https://app.hellochurch.tech/feed/'.$church['churchSlug'].'/podcast.jpg';
+						
+						$result = get_remote_file_info($img);
 
-						echo '<p>Existing Image:</p>';
-						echo '<img src="'.$img.'" alt="Podcast Image" />';
-					
+						if($result['fileExists']>0){
+							echo '<p>Existing Image:</p>';
+							echo '<img src="'.$img.'" alt="" />';
+						}
 					?>
 					<label>Image must be a square JPG file and at least 1920px by 1920px in size</label> 
 					<input id="file" type="file" name="file" />
