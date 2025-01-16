a57dafd8ba162bc6b55398bef60b514307a36ae9
Jack Barber <jack@hellotechnology.co.uk>
2025-01-16 16:31:06 +0000

    Update communication_send_email.php

--- admin/templates/pages/communication/communication_send_email.php
+++ admin/templates/pages/communication/communication_send_email.php
@@ -118,6 +118,37 @@ foreach($email as $type => $item){
         
         }
 	}
+	if($type=='image'){
+		$image = hello_church_file($item);
+		$emailContent .= '<img src="https://app.hellochurch.tech/feed/file-image/'.$image['churchID'].'/'.$image['fileID'].'" alt="Image" style="margin-bottom:16px;" />';
+	}
+	if($type=='file'){
+		$file = hello_church_file($item);
+		$emailContent .= '<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; box-sizing: border-box; width: 100%; min-width: 100%;" width="100%">
+                    <tbody>
+                      <tr>
+                        <td align="left" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; padding-bottom: 16px;" valign="top">
+                          <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
+                            <tbody>
+                              <tr>
+                                <td style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; border-radius: 4px; text-align: center; background-color: #142c8e;" valign="top" align="center" bgcolor="#0867ec"> <a href="https://app.hellochurch.tech/feed/file/'.$file['churchID'].'/'.$file['fileID'].'" target="_blank" style="border: solid 2px #142c8e; border-radius: 4px; box-sizing: border-box; cursor: pointer; display: inline-block; font-size: 16px; font-weight: bold; margin: 0; padding: 12px 24px; text-decoration: none; text-transform: capitalize; background-color: #142c8e; border-color: #142c8e; color: #ffffff;">Download: '.$file['fileName'].'</a> </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>';
+	}
+	if($type=='event'){
+		$parts = explode("_", $item);
+		$event = $HelloChurchEvents->event($parts[0]);
+		$tParts = explode(" ", $parts[1]);
+		$dParts = explode("-", $tParts[0]);
+		$timeStamp = "$dParts[2]/$dParts[1]/$dParts[0] $tParts[1]";
+		$emailContent .= '<div style="font-family: Helvetica, sans-serif !important; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;background:#f0f2f9;padding:16px;"><h2 style="font-family: Helvetica, sans-serif; font-size: 20px; font-weight: strong; margin: 0; margin-bottom: 16px;">'.$event['eventName'].'</h2><p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; font-style:italic; margin: 0; margin-bottom: 16px;">'.$timeStamp.'</p><p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">'.nl2br($event['eventDescription']).'</p></div>';
+
+	}
 	
 }
 
