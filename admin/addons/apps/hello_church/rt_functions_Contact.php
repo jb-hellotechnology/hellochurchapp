0c25c9359c1697925d38454e4d0b94c36e6cc5e2
Jack Barber <jack@hellotechnology.co.uk>
2025-01-21 22:05:38 +0000

    s

--- admin/addons/apps/hello_church/rt_functions_Contact.php
+++ admin/addons/apps/hello_church/rt_functions_Contact.php
@@ -30,7 +30,6 @@
 			$owner = $HelloChurchContacts->check_owner(perch_member_get('churchID'), $contactID);
 			if($owner){
 				$contact = $HelloChurchContacts->find($contactID);
-				print_r($contact);
 		        $contact->delete(); 
 			}
 		}
