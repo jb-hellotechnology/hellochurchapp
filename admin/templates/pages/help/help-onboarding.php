b4e0c7e8be4c78ad941ca485c68c99b5fa9f5394
Jack Barber <jack@hellotechnology.co.uk>
2025-01-21 14:02:48 +0000

    Update help-onboarding.php

--- admin/templates/pages/help/help-onboarding.php
+++ admin/templates/pages/help/help-onboarding.php
@@ -16,6 +16,10 @@ perch_layout('header');
 	?>
 	<h1>Onboarding</h1>
 	<p>Need some advice? Want help importing your contacts? Book a session with Jack:
-	<iframe frameborder="0" width="100%" height="720" src="https://meet.brevo.com/hello-church-tech/borderless?l=hello-church-onboarding"></iframe>
+	<!-- static button start -->
+	<link href="https://meetings.brevo.com/assets/styles/popup.css" rel="stylesheet" />
+	<script src="https://meetings.brevo.com/assets/libs/popup.min.js" type="text/javascript"></script>
+	<a href="" onclick="BrevoBookingPage.initStaticButton({ url: 'https://meet.brevo.com/hello-church-tech/borderless?l=hello-church-onboarding' });return false;" style="cursor: pointer; font-family: Inter; font-weight: 500; background-color: #1B1B1B; color: white; padding: 0.5rem 1rem; border: 0px; box-shadow: rgba(0, 0, 0, 0.15) 0px -2px 0px inset; border-radius: 16px; text-decoration: none; display: inline-block;">Book Now</a>
+	<!-- static button end -->
 </main>
 <?php perch_layout('footer'); ?>
