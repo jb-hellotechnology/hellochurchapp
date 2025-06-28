43454bda9945fdb5fb5dc5ede2ce4d781ca97d6a
Jack Barber <80117647+jb-hellotechnology@users.noreply.github.com>
2025-06-28 20:04:33 +0100

    training style

--- admin/templates/pages/training/session.php
+++ admin/templates/pages/training/session.php
@@ -6,7 +6,7 @@ include('../../../secrets.php');
 perch_layout('header_public');
 
 ?>
-<main class="flow public narrow">
+<main class="flow public narrow training">
 	<?php
 		public_session(perch_get('church'), perch_get('session'));
 	?>
