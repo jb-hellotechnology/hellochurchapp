e833e33ac29487cb827f503909fad2c88027db6d
Jack Barber <80117647+jb-hellotechnology@users.noreply.github.com>
2025-08-14 19:59:42 +0100

    Update contacts_edit_contact.php

--- admin/templates/pages/contacts/contacts_edit_contact.php
+++ admin/templates/pages/contacts/contacts_edit_contact.php
@@ -100,6 +100,29 @@ $name = hello_church_contact_get(perch_get('id'), 'contactFirstName').' '.hello_
 					<button id="upload" class="button primary">Upload</button>
 				</footer>
 			</section>
+			<section>
+				<div id="mapdiv" style="height:200px;width:100%;"></div>
+				<script src="https://www.openlayers.org/api/OpenLayers.js"></script>
+				<script>
+					map = new OpenLayers.Map("mapdiv");
+					map.addLayer(new OpenLayers.Layer.OSM());
+					
+					var lonLat = new OpenLayers.LonLat( -0.1279688 ,51.5077286 )
+						.transform(
+							new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+							map.getProjectionObject() // to Spherical Mercator Projection
+					);
+					
+					var zoom=16;
+					
+					var markers = new OpenLayers.Layer.Markers( "Markers" );
+					map.addLayer(markers);
+					
+					markers.addMarker(new OpenLayers.Marker(lonLat));
+					
+					map.setCenter (lonLat, zoom);
+				</script>
+			</section>
 		</div>
 	</div>
 	
